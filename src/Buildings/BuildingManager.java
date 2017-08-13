658eb10d9ef562c79a7afd12a643890e91cce368
Millan Singh <millansingh@gmail.com>
2017-08-13 15:31:31 -0700

    Scaffolding for buildings, farm

--- src/Buildings/BuildingManager.java
+++ src/Buildings/BuildingManager.java
@@ -1,5 +1,24 @@
 package Buildings;
 
-public class BuildingManager {
+import java.util.ArrayList;
 
+import Resources.ResourceManager;
+
+public class BuildingManager {
+	
+	private ResourceManager resources;
+
+	private ArrayList<Building> homes = new ArrayList<Building>();
+	private ArrayList<Building> farms = new ArrayList<Building>();
+	private ArrayList<Building> lumberyards = new ArrayList<Building>();
+	private ArrayList<Building> markets = new ArrayList<Building>();
+	
+	public BuildingManager(ResourceManager res) {
+		resources = res;
+	}
+	
+	public int farm() {
+		
+	}
+	
 }
---
+++ src/Buildings/Farm.java
@@ -0,0 +1,11 @@
+package Buildings;
+
+public class Farm extends Building {
+	
+	private int foodProduced;
+	
+	public Farm() {
+		
+	}
+
+}
