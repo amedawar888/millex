273c133697be04635439a934c7cf7ff29b9531e4
Millan Singh <millansingh@gmail.com>
2018-06-24 15:09:01 -0700

    Implements hashmap for resources in ResourceManager

--- src/Resources/ResourceManager.java
+++ src/Resources/ResourceManager.java
@@ -1,13 +1,18 @@
 package Resources;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ResourceManager {
 
 	private int food = 0;
 	private int lumber = 0;
 	private int trade = 0;
 	
+	private Map<String, Integer> resources = new HashMap<>();
+	
 	public ResourceManager(int f) {
-		food = f;
+		resources.put("food", f);
 	}
 	
 	public int getFood() {
@@ -28,6 +33,27 @@ public class ResourceManager {
 	
 	public void removeFood(int f) {
 		food = Math.max(food-f, 0);
+	}
+	
+	public int getResource(String key) {
+		try {
+			return resources.get(key);
+		} catch(Exception e) {
+			System.out.println(e);
+			return 0;
+		}
+	}
+	
+	public boolean adjustResource(String key, int amount) {
+		try {
+			int res = resources.get(key);
+			resources.put(key, res + amount);
+			return true;
+			
+		} catch(Exception e) {
+			System.out.println(e);
+			return false;
+		}
 	}
 	
 }
