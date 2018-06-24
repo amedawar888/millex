5dacc302b2ec93dbee6e405bcd9a79faa7cedb84
Millan Singh <millansingh@gmail.com>
2018-06-24 15:14:30 -0700

    Removes variables from ResourceManager that we don't need anymore

--- src/Resources/ResourceManager.java
+++ src/Resources/ResourceManager.java
@@ -4,35 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ResourceManager {
-
-	private int food = 0;
-	private int lumber = 0;
-	private int trade = 0;
 	
 	private Map<String, Integer> resources = new HashMap<>();
 	
 	public ResourceManager(int f) {
 		resources.put("food", f);
-	}
-	
-	public int getFood() {
-		return food;
-	}
-	
-	public int getLumber() {
-		return lumber;
-	}
-	
-	public int getTrade() {
-		return trade;
-	}
-	
-	public void addFood(int f) {
-		food += f;
-	}
-	
-	public void removeFood(int f) {
-		food = Math.max(food-f, 0);
 	}
 	
 	public int getResource(String key) {
