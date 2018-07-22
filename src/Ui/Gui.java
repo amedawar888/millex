220b4bb4c7656b8b525db07020832e3b2bb3c84d
amedawar888 <amedawar888@gmail.com>
2018-07-22 14:19:28 -0700

    Creates GUI Layout

--- src/Ui/Gui.java
+++ src/Ui/Gui.java
@@ -1,7 +1,19 @@
 package Ui;
 
+import java.awt.FlowLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Gui extends JPanel {
-	
+
+	public Gui() {
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		JPanel titlePanel = new JPanel(new FlowLayout());
+		titlePanel.add(new JLabel("City of Millex"));
+		this.add(titlePanel);
+	}
 }
+
+
