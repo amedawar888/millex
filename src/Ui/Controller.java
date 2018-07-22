80c31b581fa6af6f22310af7d921e623bbe6dc87
amedawar888 <amedawar888@gmail.com>
2018-07-22 14:40:27 -0700

    Creates population label

--- src/Ui/Controller.java
+++ src/Ui/Controller.java
@@ -19,6 +19,8 @@ public class Controller {
 		
 		ui = new Gui();
 		
+		ui.update(game.exportPopulation());
+		
 		window.getContentPane().add(ui);
 		
 		window.setVisible(true);
--- src/Ui/Gui.java
+++ src/Ui/Gui.java
@@ -1,6 +1,7 @@
 package Ui;
 
 import java.awt.FlowLayout;
+import java.util.HashMap;
 
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
@@ -8,11 +9,27 @@ import javax.swing.JPanel;
 
 public class Gui extends JPanel {
 
+	private JLabel popLabel;
+	
 	public Gui() {
+		
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
 		JPanel titlePanel = new JPanel(new FlowLayout());
 		titlePanel.add(new JLabel("City of Millex"));
+		
+		popLabel = new JLabel("Population: -- males, -- females");
+		JPanel popPanel = new JPanel(new FlowLayout());
+		popPanel.add(popLabel);
+		
 		this.add(titlePanel);
+		this.add(popPanel);
+	}
+	
+	public void update(HashMap<String, Integer> pop) {
+		popLabel.setText("Population: " + pop.get("males") + " males, " + pop.get("females") + " females");
+		
+		this.repaint();
 	}
 }
 
